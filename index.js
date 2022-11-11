1ae3195e62523e984420627fa5e99a9f92186708
Mark Turner <mark.edward.turner@gmail.com>
2022-11-11 11:03:23 -0500

    Commit to save progress

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const markdowngenerator = require('./develop/generatemarkdown');
+
+// Array of questions for user input
 const questions = [];
 
+inquirer 
+    .prompt([
+{
+  type: 'input',
+  message: 'What is the project title?'      
+  name: 'title',
+
+
+},
+])
+.then((response) =>
+    response
+
+
+)
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err){
+            return console.log(err)
+        }else {
+            console.log("success")
+        }
+
+    })
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
